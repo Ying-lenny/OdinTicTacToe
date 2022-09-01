a2c130e6612a68e7ede0ef6c9befebdd5e61befd
Owen Lenihan <owenlenihan101@gmail.com>
2022-09-01 10:42:32 +0100

    Added basic functionality to add Xs to board

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hi")
+
+const gameTile = document.querySelectorAll('.tile');
+
+gameTile.forEach((tile) =>
+    tile.addEventListener('click', () => {
+        markTile(tile);
+    })
+)
+
+function markTile(tile) {
+    if (tile.textContent === "") {
+        tile.textContent = "X"
+    } else {
+        console.log("Already taken bud")
+    }
+}
